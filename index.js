79d23f764c94f6f3fe9f0e947f83d53821798ecc
tarunreddybabi <tarunreddybabi@gmail.com>
2024-02-16 17:28:48 +0530

    sum of ages node task

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express=require("express")
-const app=express()
-app.use(express.json())
+const express = require("express");
+const app = express();
 
-app.get("/login",(req,res)=>{
-    res.send("hello")
+const data=[{age:"25"},{age:"26"},{age:"27"}]
+
+app.get("/sum",(req,res)=>{
+    const sum=data.reduce((total,value)=>total+parseInt(value.age),0);
+    res.json({totalAge:sum})
 })
 
-app.listen(2001,()=>{
-    console.log("server running")
+app.listen(2002,()=>{
+    console.log("server is running")
 })
